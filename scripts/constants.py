8418ee26e46db5a0669411cc17d8306df7b4c321
abhirup.ghosh <abhirup.ghosh@emmy-sharing.de>
2023-11-03 18:16:00 +0100

    updated variables list

---
+++ scripts/constants.py
@@ -0,0 +1,35 @@
+numerical = [
+            'patientid',
+            'age',
+            'scheduleddayWeek',
+            'scheduleddayDay',
+            'scheduleddayDayofyear',
+            'scheduleddayHour',
+            'scheduleddayMinute',
+            'scheduleddaySecond',
+            'appointmentdayDay',
+            'appointmentdayDayofyear'
+            ]
+
+categorical = [
+                'gender',
+                'scholarship',
+                'hypertension',
+                'diabetes',
+                'alcoholism',
+                'handcap',
+                'sms_received',
+                'scheduleddayYear',
+                'scheduleddayMonth',
+                'scheduleddayDayofweek',
+                'scheduleddayIs_month_end',
+                'scheduleddayIs_month_start',
+                'scheduleddayIs_quarter_end',
+                'scheduleddayIs_quarter_start',
+                'appointmentdayMonth',
+                'appointmentdayWeek',
+                'appointmentdayDayofweek',
+                'appointmentdayIs_month_end',
+                'appointmentdayIs_month_start',
+                'neighbourhood'
+            ]
